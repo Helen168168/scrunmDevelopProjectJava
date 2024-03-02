977917d47da3a08b7548459b14d9e26ddc084660
pengxuemei <1093232495@qq.com>
2024-03-02 18:33:09 +0800

    init project and add generate prime of 3 versions

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.uniTest.GeneratePrimes1;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        GeneratePrimes1 generatePrimes = new GeneratePrimes1();
+        generatePrimes.GeneratePrimes(4);
+
+    }
+}
