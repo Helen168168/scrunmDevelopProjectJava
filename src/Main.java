3aa5087d19916e40cb77220fac92412366253382
pengxm <pengxm@nengfeng.com.cn>
2024-03-15 17:42:30 +0800

    application of ocp

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-import org.uniTest.GeneratePrimes1;
+import org.primes.GeneratePrimes1;
+import org.org.LSubstiPrinciple.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("计算素数");
         GeneratePrimes1 generatePrimes = new GeneratePrimes1();
         generatePrimes.GeneratePrimes(4);
 
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(new Square(4));
+        shapes.add(new Rectangle(3,5));
+        new Client().calculateTotalArea(shapes);
     }
 }
---
+++ src/org/primes/GeneratePrimes3.java
@@ -0,0 +1,49 @@
+package org.primes;
+
+public class GeneratePrimes3 {
+    private static boolean[] isCrossed;
+    private static int[] result;
+    public static int[] GeneratePrimes(int maxValue) {
+        if(maxValue < 2) {
+            return new int[0];
+        } else {
+            initializeArrayOfIntegers(maxValue);
+            crossOutMultiples();
+            getResult();
+            return result;
+        }
+    }
+    public static void getResult() {
+        int i;
+        int j;
+        int count = 0;
+        for (i = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {
+                count++;
+            }
+        }
+        result = new int[count];
+        for (i = 0, j = 0; i < isCrossed.length; i++) {
+            if (isCrossed[i]) {
+                result[j++] = i;
+            }
+        }
+    }
+    public static void crossOutMultiples() {
+        int j;
+        int i;
+        for ( i = 2; i < Math.sqrt(isCrossed.length) + 1; i++) {
+
+            for (j =  2 * i; j < isCrossed.length; j += i) {
+                isCrossed[j] = false;
+            }
+        }
+    }
+
+    public static void initializeArrayOfIntegers(int maxValue) {
+        isCrossed = new boolean[maxValue + 1];
+        for ( int i = 2; i < isCrossed.length; i++) {
+            isCrossed[i] = false;
+        }
+    }
+}
